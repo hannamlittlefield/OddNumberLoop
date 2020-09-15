0cfce7d51b915e41201660a39432c1313d6f82cd
Hanna Littlefield <hannamlittlefield@gmail.com>
2020-09-15 17:00:28 -0400

    correct code updated

--- Loop.js
+++ Loop.js
@@ -1,4 +1,5 @@
 //for loop returning 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 - increments by every odd # (1, 3, 5, 7, 9, etc)
-for (var i = 0, i < 100,i++) {
-Array.push(i)
-};
+for (var i = 1; i <= 10; i++) {
+    console.log(Math.pow(i, 2));
+
+}
